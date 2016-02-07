a33279e59088d74837b4af7ce33a3176f53d3768
dolezmat <matej.dolezal@cdn77.com>
2016-02-07 23:39:11 +0100

    weather API key

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-var FORCAST_API_KEY = "{YOUR_KEY_HERE}";
+var FORCAST_API_KEY = "15abfd144c70c08571481dc8a72d41ab";
 var HUE_BASE = "http://{IP_ADDRESS_OF_BRIDGE}/api/{Username}/";
 var PERSONAL_CALENDAR = [];
